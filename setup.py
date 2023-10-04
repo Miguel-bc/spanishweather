498141f55c8744a377f5fc7977521af3d800b9ef
Miguel-bc <miguel.blaya@gmail.com>
2023-10-04 20:47:34 +0200

    nueva version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spanishweather",
-    version="0.1.3",
+    version="0.1.4",
     description="Una aplicación para obtener datos meteorológicos de la AEMET.",
     author="Miguel Blaya",
     author_email="miguel.blaya@gmail.com",
